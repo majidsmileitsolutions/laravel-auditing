c50a49df4bce3651cfe15cce25906b68338b6b8c
Majid Alaeinia <alaeinia.majid@gmail.com>
2023-10-30 13:09:52 +0330

    [IU-511] Add 'RabbitAuditConsumerCommand'

---
+++ src/Console/Rabbit/RabbitAuditConsumerCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace OwenIt\Auditing\Console\Rabbit;
+
+use OwenIt\Auditing\Console\BaseCommand;
+use OwenIt\Auditing\Constants\ElasticsearchIndices;
+use OwenIt\Auditing\Constants\RabbitQueues;
+use OwenIt\Auditing\Models\Audit;
+use Bschmitt\Amqp\Facades\Amqp;
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+
+class RabbitAuditConsumerCommand extends BaseCommand
+{
+    private Audit $audit;
+    private string $queue;
+    private string $elasticIndex;
+
+    public function __construct(Audit $audit)
+    {
+        parent::__construct();
+        $this->audit = $audit;
+        $this->queue = RabbitQueues::AUDITS_RABBIT_QUEUE;
+        $this->elasticIndex = ElasticsearchIndices::AUDITS;
+    }
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:rabbit-consumer-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Consume audits rabbit messages.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        try {
+            Amqp::consume($this->queue, function ($message, $resolver) {
+                $auditArray = json_decode($message->body, true);
+                DB::beginTransaction();
+                $auditArray['is_queued'] = true;
+                $auditArray['is_acked'] = true;
+                Http::post(config('elasticsearch.base_url') . "/$this->elasticIndex/_doc", $auditArray);
+                $resolver->acknowledge($message);
+                $this->audit->query()->where('id', $auditArray['id'])->update([
+                    'is_acked' => true,
+                ]);
+                DB::commit();
+                $resolver->stopWhenProcessed();
+            });
+
+            return SymfonyCommand::SUCCESS;
+        } catch (Exception $exception) {
+            $this->error($exception->getMessage());
+
+            return SymfonyCommand::FAILURE;
+        }
+    }
+}
