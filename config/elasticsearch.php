141a984eabb9ccc34dc3d59e29e7d305cd7bb603
Majid Alaeinia <alaeinia.majid@gmail.com>
2023-11-06 11:11:44 +0330

    [IU-511] Add default value for 'audit' index env values

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -2,5 +2,5 @@
 
 return [
     'base_url' => env('ELASTIC_HOST', 'http://elasticsearch:9200'),
-    'audits_index' => env('APP_NAME').'_'.env('APP_ENV').'_'.env('audits'),
+    'audits_index' => env('APP_NAME', 'your_app_name').'_'.env('APP_ENV', 'your_app_env').'_'.env('audits'),
 ];
