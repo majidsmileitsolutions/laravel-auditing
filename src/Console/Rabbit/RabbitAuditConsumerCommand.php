85c19aada6e0662f7c1abe8ea015a1b07b9ff8d1
Majid Alaeinia <alaeinia.majid@gmail.com>
2023-11-13 21:58:10 +0330

    [IU-511] Consider elasticsearch response

--- src/Console/Rabbit/RabbitAuditConsumerCommand.php
+++ src/Console/Rabbit/RabbitAuditConsumerCommand.php
@@ -4,10 +4,13 @@ namespace OwenIt\Auditing\Console\Rabbit;
 
 use Bschmitt\Amqp\Facades\Amqp;
 use Exception;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use OwenIt\Auditing\Console\BaseCommand;
 use OwenIt\Auditing\Models\Audit;
+use RuntimeException;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 
 class RabbitAuditConsumerCommand extends BaseCommand
@@ -16,12 +19,15 @@ class RabbitAuditConsumerCommand extends BaseCommand
 
     private string $queue;
 
+    private string $elasticBaseUrl;
+
     private string $elasticIndex;
 
     public function __construct(Audit $audit)
     {
         parent::__construct();
         $this->audit = $audit;
+        $this->elasticBaseUrl = config('elasticsearch.base_url');
         $this->queue = config('amqp.audit_queue', 'audit_queue');
         $this->elasticIndex = config('elasticsearch.audits_index', 'your_app_name_your_env_audits');
     }
@@ -51,7 +57,14 @@ class RabbitAuditConsumerCommand extends BaseCommand
                 DB::beginTransaction();
                 $auditArray['is_queued'] = true;
                 $auditArray['is_acked'] = true;
-                Http::post(config('elasticsearch.base_url')."/$this->elasticIndex/_doc", $auditArray);
+                $elasticResponse = Http::post($this->elasticBaseUrl."/$this->elasticIndex/_doc", $auditArray);
+                if ($elasticResponse->status() !== Response::HTTP_CREATED) {
+                    Log::error('failed_to_index_document', [
+                         'status_code' => $elasticResponse->status(),
+                         'response' => $elasticResponse->json(),
+                    ]);
+                    throw new RuntimeException('failed_to_index_document');
+                }
                 $resolver->acknowledge($message);
                 $this->audit->query()->where('id', $auditArray['id'])->update([
                     'is_acked' => true,
