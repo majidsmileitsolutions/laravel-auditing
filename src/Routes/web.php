15451cdd688fd8caef47732f9d23dea7be5c9bff
Majid Alaeinia <alaeinia.majid@gmail.com>
2023-11-24 12:12:02 +0330

    [WIP]

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use OwenIt\Auditing\Http\Controllers\AuditSearchController;
 
-Route::middleware(config('audit.search_middleware_name'))
-     ->get('admin/audits/search', [AuditSearchController::class, 'index'])
-     ->name('admin.audits.search');
+//Route::middleware(config('audit.search_middleware_name'))
+//     ->get('admin/audits/search', [AuditSearchController::class, 'index'])
+//     ->name('admin.audits.search');
+
+Route::get('admin/audits/search', function () {
+    echo 'hihihi';
+});
