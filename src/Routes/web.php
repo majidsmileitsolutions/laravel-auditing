29351d8e82711855bae29618c344c8249800afef
Majid Alaeinia <alaeinia.majid@gmail.com>
2023-11-24 11:33:35 +0330

    [WIP]

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use OwenIt\Auditing\Http\Controllers\AuditSearchController;
 
-// 'auth:admin-api'
 Route::middleware(config('audit.search_middleware_name'))
      ->get('admin/audits/search', [AuditSearchController::class, 'index'])
      ->name('admin.audits.search');
